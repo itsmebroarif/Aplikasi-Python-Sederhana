2b1ff8be88c59cd1e2745e02e0fd843db675c94f
itsmebroarif.dev <68406328+itsmebroarif@users.noreply.github.com>
2021-10-12 08:19:04 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,19 @@
 dataX = input()
 dataY = input()
 dataZ = input()
+
 print("------------------")
 print(dataX)
 print(dataX,dataY)
 print(dataX,dataY,dataZ)
 print(dataY,dataZ)
 print(dataZ)
+
 print("------------------")
 print(dataX)
 print(dataX,dataY)
 print(dataX,dataY,dataZ)
+
 print("------------------")
 print(dataX,dataY,dataZ)
 print(dataX,dataY,dataZ)
@@ -22,6 +25,7 @@ print("------------------")
 # If, Elif, Else 
 
 print("====================")
+
 questA = input("10+20 = ")
 questB = input("10+20 = ")
 
@@ -35,12 +39,47 @@ else:
     ("-")
 
 #Function
-#public model view => y
+#public model view (True)#
 
 def myFunction(fname):
     print(fname + " XII RPL 3")
 myFunction("Arif")
 
-def myPublicFunction(sname):
-    print("Hallo" + sname " From XII RPL 3")
-myPublicFunction("Arif")
+# input environment
+
+eventPertama = print("Training pertama menggunakan python 3")
+eventKedua = print("Silahkan Input data dibawah ini untuk membuat print data")
+
+print("--------------------------------------------------")
+triggerPertama = input("nama : ")
+print("--------------------------------------------------")
+triggerKedua = input("asal : ")
+print("--------------------------------------------------")
+triggerKetiga = input("tempat tanggal lahir : ")
+print("--------------------------------------------------")
+triggerKeempat = input("Pengalaman Bekerja : ")
+print("--------------------------------------------------")
+triggerKelima = input("Pendidikan Terakhir")
+print("--------------------------------------------------")
+triggerKeenam = input("Jurusan Pendidikan Terakhir : ")
+print("--------------------------------------------------")
+
+
+# Table & output Environment
+print("==================================================")
+print(triggerPertama)
+print("==================================================")
+print(triggerKedua)
+print("==================================================")
+print(triggerKetiga)
+print("==================================================")
+print(triggerKeempat)
+print("==================================================")
+print(triggerKelima)
+print("==================================================")
+print(triggerKeenam)
+print("==================================================")
+print("==================================================")
+# footer 
+print("Created By Arif Alexander")
+print("==================================================")
